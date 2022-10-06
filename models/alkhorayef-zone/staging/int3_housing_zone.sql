6a5ac25bd00a27f2425189d2b15d8bbf2195868d
Pouria Modaresi <pouria@ifiveo.com>
2022-10-06 16:07:19 +0000

    capacity

--- models/alkhorayef-zone/staging/int3_housing_zone.sql
+++ models/alkhorayef-zone/staging/int3_housing_zone.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='ephemeral') }}
 with cte as ( select * 
-FROM  {{ ref('int2_time_zone')}} where class_type='housing'and id is not null)
-select min(col_timestamp)as event_time,
+FROM  {{ ref('int2_time_zone')}} where class_type='housing'and id is not null),
+cte1 as (select min(col_timestamp)as event_time,
 date,hour,cast(count(*)/60 as float) as total_minute,
 shift, 
 id  ,
@@ -9,3 +9,15 @@ zone
 from cte 
 group by date,hour,zone,shift,id
 having id >0 and total_minute>0
+order by zone ,event_time),
+cte2 as (select  *,sum(total_minute)over (partition by date,hour,shift,zone)  as sum_total_minute
+FROM  cte1),
+cte3 as ( select  *,count(id)over (partition by date,hour,shift,zone) as count_housing  from cte2 )
+select event_time,date,hour,shift,
+case 
+  when zone='zone1' then 'station1'
+  when zone='zone2' then 'station2'
+  when zone='zone3' then 'station3'
+  when zone='zone4' then 'station4'
+  else '0' end as station,id,total_minute,sum_total_minute,count_housing ,
+sum_total_minute/count_housing  as capacity from cte3
--- models/alkhorayef-zone/staging/int3_person_zone.sql
+++ models/alkhorayef-zone/staging/int3_person_zone.sql
@@ -1,11 +1,20 @@
 {{ config(materialized='ephemeral') }}
 with cte as ( select * 
-FROM  {{ ref('int2_time_zone')}} where class_type='person'and id is not null)
-select min(col_timestamp)as event_time,
+FROM  {{ ref('int2_time_zone')}} where class_type='person'and id is not null),
+cte1 as (select min(col_timestamp)as event_time,
 date,hour,cast(count(*)/60 as float) as total_minute,
 shift, 
 id ,
 zone  
 from cte 
 group by date,hour,zone,shift,id
-having id >0 and total_minute>0
+having id >0 and total_minute>0),
+
+cte2 as (select event_time,date,hour,shift,case 
+  when zone='zone1' then 'station1'
+  when zone='zone2' then 'station2'
+  when zone='zone3' then 'station3'
+  when zone='zone4' then 'station4'
+  else '0' end as station,id,total_minute,count(id) over (partition by date,hour,shift,zone) as person_count  from cte1)
+select  *,sum(total_minute)over (partition by date,hour,shift,station)  as sum_total_minute
+FROM  cte2 
